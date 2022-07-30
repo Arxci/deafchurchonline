04d49962866c291370a2715ba4a17339db3cdaed
Garrett <garretthumbert9@gmail.com>
2022-07-30 14:12:44 -0500

    home about page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ButtonCTA from '../global/ButtonCTA'
 import Banner from './home/Banner'
 
 export default function Home({setCurrentHeaderIndex}) {
+  const [aboutImgFocus, setAboutImgFocus] = useState(false);
+
   useEffect(() => {
     setCurrentHeaderIndex(1)
   })
 
+  function onUpdateAIF() {
+    setAboutImgFocus(!aboutImgFocus);
+  }
+
+  function onFadeIn() {
+    setAboutImgFocus(true);
+  }
+
+  function onFadeOut() {
+    setAboutImgFocus(false);
+  }
+
   return (
     <div className='home'>
-        <Banner title={'ANNOUNCEMENTS'} subtext={'            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab modi quisquam ullam tenetur, illum maiores, officia vitae eaque ipsam facilis possimus, distinctio molestiae labore soluta ratione nam alias molestias. Voluptate molestias amet numquam tenetur sunt, at aspernatur quasi illo voluptatum consequatur deleniti saepe earum. Tempore dignissimos soluta molestias ipsum fuga. Dolores, dolorum dolorem debitis ad non perferendis quisquam natus omnis labore obcaecati nulla? Aspernatur eos quam obcaecati labore laborum enim magni blanditiis aliquam, qui commodi tempore ex error pariatur assumenda ea libero nobis eius, repudiandae, ratione optio quas? Veritatis quia excepturi repellendus, voluptas ad iusto omnis amet accusamus modi veniam.'} buttonLink={"#"} buttonText={'Learn More'}/>
+        <Banner title={'ANNOUNCEMENTS'} subtext={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab modi quisquam ullam tenetur, illum maiores, officia vitae eaque ipsam facilis possimus, distinctio molestiae labore soluta ratione nam alias molestias. Voluptate molestias amet numquam tenetur sunt, at aspernatur quasi illo voluptatum consequatur deleniti saepe earum. Tempore dignissimos soluta molestias ipsum fuga. Dolores, dolorum dolorem debitis ad non perferendis quisquam natus omnis labore obcaecati nulla? Aspernatur eos quam obcaecati labore laborum enim magni blanditiis aliquam, qui commodi tempore ex error pariatur assumenda ea libero nobis eius, repudiandae, ratione optio quas? Veritatis quia excepturi repellendus, voluptas ad iusto omnis amet accusamus modi veniam.'} buttonLink={"#"} buttonText={'Learn More'}/>
         <div className='container__S'>
           <div className='home__title'>
             <span>
@@ -22,8 +37,28 @@ export default function Home({setCurrentHeaderIndex}) {
             </span>
           </div>
         </div>
-        <div className='home__about'>
+        <div className='home__about padding__aL'>
           <div className='container__S'>
+            <ul className='home__about__list'>
+              <li className='home__about__left'>
+                <div className='home__about__subtitle'>
+                  About The Church
+                </div>
+                <div className='home__about__title'>
+                  Deaf Church Online is an online church for deaf and hard of hearing people and their loved ones.
+                </div>
+                <div className='home__about__text'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore omnis, dolor tempore sed officia repellat, cumque voluptatem et quod tempora ipsa natus ut iusto id sapiente ratione. Hic, deserunt facilis! Sint veritatis, exercitationem ullam delectus ipsum praesentium architecto nobis nihil sed vero molestias facere maxime amet ducimus dolore ipsam ut accusamus enim obcaecati, voluptatum tenetur nulla. Architecto quo facilis dolorum ex itaque nobis, suscipit recusandae nisi similique sunt vitae, quibusdam optio iste sequi? Recusandae impedit magni odio voluptatum atque, blanditiis aspernatur sequi nihil. Perspiciatis repellendus, doloremque blanditiis error quis illo optio deserunt quibusdam asperiores distinctio qui repudiandae repellat magnam possimus!
+                </div>
+              </li>
+              <li className='home__about__right'>
+                <div className={aboutImgFocus ? 'home__about__imageBTN has-fade fade-in' : 'home__about__imageBTN has-fade fade-out'} onBlur={() => onFadeOut()} onFocus={() => onFadeIn()} onMouseEnter={() => onFadeIn()} onMouseLeave={() => onFadeOut()}>
+                  <div>
+                    <ButtonCTA buttonLink={'/about'} buttonText={'LEARN MORE'}/>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
     </div>
