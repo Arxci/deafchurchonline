86b44f1232eb790bcedcdb489eb7ea6bf1a351b0
Garrett <garretthumbert9@gmail.com>
2022-07-30 21:16:41 -0500

    fixed again

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -25,17 +25,19 @@ export default function Home({setCurrentHeaderIndex}) {
   return (
     <div className='home'>
         <Banner title={'ANNOUNCEMENTS'} subtext={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab modi quisquam ullam tenetur, illum maiores, officia vitae eaque ipsam facilis possimus, distinctio molestiae labore soluta ratione nam alias molestias. Voluptate molestias amet numquam tenetur sunt, at aspernatur quasi illo voluptatum consequatur deleniti saepe earum. Tempore dignissimos soluta molestias ipsum fuga. Dolores, dolorum dolorem debitis ad non perferendis quisquam natus omnis labore obcaecati nulla? Aspernatur eos quam obcaecati labore laborum enim magni blanditiis aliquam, qui commodi tempore ex error pariatur assumenda ea libero nobis eius, repudiandae, ratione optio quas? Veritatis quia excepturi repellendus, voluptas ad iusto omnis amet accusamus modi veniam.'} buttonLink={"#"} buttonText={'Learn More'}/>
-        <div className='container__S'>
-          <div className='home__title' data-aos="fade-up">
-            <span>
-              WELCOME TO SOUTHERN
-            </span>
-            <span>
-              DEAF
-            </span>
-            <span>
-              FELLOWSHIP
-            </span>
+        <div className='home__title'>
+          <div className='container__S'>
+            <div className='home__title' data-aos="fade-up">
+              <span>
+                WELCOME TO SOUTHERN
+              </span>
+              <span>
+                DEAF
+              </span>
+              <span>
+                FELLOWSHIP
+              </span>
+            </div>
           </div>
         </div>
         <div className='home__about'>
